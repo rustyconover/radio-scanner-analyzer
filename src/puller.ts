0f78e1708d05f3e134526b3c66a63503b1172a76
Rusty Conover <rusty@conover.me>
2021-01-15 08:49:59 -0500

    Add better error handling around ffmpeg

--- src/puller.ts
+++ src/puller.ts
@@ -28,7 +28,7 @@ let config: MicroWriterOptions | undefined;
  *
  * @param data the data received from the streaming server
  */
-async function analyze_data(data: Buffer, stream_description: string) {
+async function analyze_data(data: Buffer, stream_description: string): Promise<boolean> {
     const sample_rate = 16000;
 
     const audio_filename = `/tmp/scanner-audio.${(new Date()).getTime()}.${Math.round(Math.random() * 100000)}.mpeg`;
@@ -42,84 +42,88 @@ async function analyze_data(data: Buffer, stream_description: string) {
         pcm_output,
     ])
 
-    exec(makePCM, {
-        maxBuffer: 1024 * 1024 * 64,
-        encoding: null,
-    },
-        async (err: any) => {
-            fs.unlinkSync(audio_filename);
-            if (err) {
-                console.error(err)
-                return;
-            }
-
-            const loud_indexes = [0];
-            {
-                const pcm_data = fs.readFileSync(pcm_output, { encoding: null });
-                fs.unlinkSync(pcm_output);
-                const pcm_values = new Int16Array(pcm_data.buffer);
-
-                if (pcm_values.length > (window_length / 1000) * sample_rate * 1.2) {
+    return new Promise((resolve, reject) =>
+        exec(makePCM, {
+            maxBuffer: 1024 * 1024 * 64,
+            encoding: null,
+        },
+            async (err: any) => {
+                fs.unlinkSync(audio_filename);
+                if (err) {
+                    console.error(err)
+                    reject(err);
                     return;
                 }
 
-                pcm_values.forEach((v, idx) => {
-                    if (Math.abs(v) > loud_threshold) {
-                        loud_indexes.push(idx);
+                const loud_indexes = [0];
+                {
+                    const pcm_data = fs.readFileSync(pcm_output, { encoding: null });
+                    fs.unlinkSync(pcm_output);
+                    const pcm_values = new Int16Array(pcm_data.buffer);
+
+                    if (pcm_values.length > (window_length / 1000) * sample_rate * 1.2) {
+                        return;
                     }
-                });
-                loud_indexes.push(pcm_values.length - 1);
-                //                    console.log("Total samples: ", pcm_values.length);
-            };
 
-            //                console.log("Loud indexes: ", loud_indexes.length);
+                    pcm_values.forEach((v, idx) => {
+                        if (Math.abs(v) > loud_threshold) {
+                            loud_indexes.push(idx);
+                        }
+                    });
+                    loud_indexes.push(pcm_values.length - 1);
+                    //                    console.log("Total samples: ", pcm_values.length);
+                };
 
-            const diff_indexes: number[] = [];
+                //                console.log("Loud indexes: ", loud_indexes.length);
 
-            let total_silence_seconds = 0;
+                const diff_indexes: number[] = [];
 
-            if (loud_indexes.length < 2) {
-                total_silence_seconds = window_length / 1000;
-            }
-            for (let i = 1; i < loud_indexes.length; i++) {
-                const sample_dist = loud_indexes[i] - loud_indexes[i - 1];
-                if (sample_dist > (sample_rate * .25)) {
-                    diff_indexes.push(sample_dist);
-                    // console.log("Interval:", sample_dist / sample_rate);
-                    total_silence_seconds += sample_dist / sample_rate;
+                let total_silence_seconds = 0;
+
+                if (loud_indexes.length < 2) {
+                    total_silence_seconds = window_length / 1000;
                 }
-            }
-
-            total_silence_seconds = Math.min(total_silence_seconds, window_length / 1000);[]
-            //                console.log("Total silence:", total_silence_seconds);
-            const percent_silent = total_silence_seconds / (window_length / 1000);
-            console.log("Silence Percentage", percent_silent);
-
-            const active_percent = 1.0 - percent_silent;
-
-            if (config == null) {
-                const write_key = process.env["MICROPREDICTION_WRITE_KEY"];
-                if (write_key == null) {
-                    throw new Error("No MICROPREDICTION_WRITE_KEY defined");
+                for (let i = 1; i < loud_indexes.length; i++) {
+                    const sample_dist = loud_indexes[i] - loud_indexes[i - 1];
+                    if (sample_dist > (sample_rate * .25)) {
+                        diff_indexes.push(sample_dist);
+                        // console.log("Interval:", sample_dist / sample_rate);
+                        total_silence_seconds += sample_dist / sample_rate;
+                    }
                 }
 
-                config = await MicroWriterConfig.create({
-                    write_key,
-                });
+                total_silence_seconds = Math.min(total_silence_seconds, window_length / 1000);[]
+                //                console.log("Total silence:", total_silence_seconds);
+                const percent_silent = total_silence_seconds / (window_length / 1000);
+                console.log("Silence Percentage", percent_silent);
+
+                const active_percent = 1.0 - percent_silent;
+
+                if (config == null) {
+                    const write_key = process.env["MICROPREDICTION_WRITE_KEY"];
+                    if (write_key == null) {
+                        throw new Error("No MICROPREDICTION_WRITE_KEY defined");
+                    }
+
+                    config = await MicroWriterConfig.create({
+                        write_key,
+                    });
+                }
+
+                if (config != null) {
+                    const writer = new MicroWriter(config);
+
+                    const clean_description = stream_description.replace(/[ \.,\(\)\-\/]/g, '_')
+                        .replace(/_+/g, '_')
+                        .toLowerCase();
+
+                    console.log(clean_description);
+                    await writer.set(`scanner-audio-${clean_description}.json`, active_percent);
+                }
+                resolve(true);
+                return;
             }
-
-            if (config != null) {
-                const writer = new MicroWriter(config);
-
-                const clean_description = stream_description.replace(/[ \.,\(\)\-\/]/g, '_')
-                    .replace(/_+/g, '_')
-                    .toLowerCase();
-
-                console.log(clean_description);
-                await writer.set(`scanner-audio-${clean_description}.json`, active_percent);
-            }
-        }
-    )
+        ));
 }
 
 
@@ -163,7 +167,7 @@ function listenToStream(stream_id: number, description: string) {
         }
         //        console.log('headers:', res.headers);
 
-        const timeout_handler = () => {
+        const timeout_handler = async () => {
             // Make one large buffer containing all of the partial
             // response data.
             const final_data = Buffer.concat(interval_data);
@@ -173,7 +177,15 @@ function listenToStream(stream_id: number, description: string) {
             // Start a new timer for a sampling interval.
             setTimeout(timeout_handler, window_length);
             // Send the data to ffmpeg and parse out the values.
-            analyze_data(final_data, description);
+
+            try {
+                await analyze_data(final_data, description);
+            } catch (e) {
+                // Cancel this request, since the data was corrupted.
+                req.abort();
+                interval_data.splice(0);
+                setTimeout(listenToStream, 2000, stream_id, description);
+            }
         };
         setTimeout(timeout_handler, window_length);
 
