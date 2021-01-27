7473d6852195dd7c21a993b55e52bf60a7a59c02
Rusty Conover <rusty@conover.me>
2021-01-27 13:33:05 -0500

    Testing

--- src/puller.ts
+++ src/puller.ts
@@ -49,11 +49,11 @@ async function analyze_data(data: Buffer, stream_description: string): Promise<v
     // streams being analyzed at once.
     try {
         console.log(makePCM);
-        const f = execSync(makePCM, {
-            maxBuffer: 1024 * 1024 * 64,
-            encoding: null,
-        });
-        console.log(f);
+        // const f = execSync(makePCM, {
+        //     maxBuffer: 1024 * 1024 * 64,
+        //     encoding: null,
+        // });
+        // console.log(f);
         console.log("Ffmpeg finished");
     } catch (e) {
         console.error("Error running ffmpeg");
